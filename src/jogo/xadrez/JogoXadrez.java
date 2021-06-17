455a62bb97d572e9c8317be8c22f0895aae5cbd6
Edumokfa <edumokfa@hotmail.com>
2021-06-17 11:32:59 -0300

    First Commit

--- src/jogo/xadrez/JogoXadrez.java
+++ src/jogo/xadrez/JogoXadrez.java
@@ -1,16 +1,9 @@
 package jogo.xadrez;
 
-/**
- *
- * @author P. Godoy
- */
 public class JogoXadrez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
 
 }
